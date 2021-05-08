e741c7408b017fe39a572e6042d7b5fab844b9a1
Marvellous Nwachukwu <marvellousn561@gmail.com>
2021-05-08 14:09:10 +0100

    created folders for organization, and setup chakra ui

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Heading } from '@chakra-ui/react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Heading>OAKONSULT's Dummy HomePage</Heading>
+    </div>
+  );
+};
+
+export default HomePage;
