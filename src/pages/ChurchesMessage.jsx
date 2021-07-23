1974d4bdca3db6a9b81b9389b2ed417aac52644d
Marvellous Nwachukwu <marvellousn561@gmail.com>
2021-07-23 16:52:57 +0100

    changed teal code, made some element compact

--- src/pages/ChurchesMessage.jsx
+++ src/pages/ChurchesMessage.jsx
@@ -166,7 +166,7 @@ const Churches = () => {
         className='SeeRelatedArticle CTA_Btn'
         id='btn'
         style={{
-          backgroundColor: '#009ba7',
+          backgroundColor: '#009b9b',
           marginBottom: '6rem',
           marginTop: '3rem',
         }}
--- src/pages/TrainingAndResourcing/TrainingAndResourcing.jsx
+++ src/pages/TrainingAndResourcing/TrainingAndResourcing.jsx
@@ -153,15 +153,15 @@ const TrainingAndResourcing = () => {
           <p>The BYS Course seeks to:</p>
           <ul>
             <li>
-              provide a deeper understanding of the disability community and the
+              Provide a deeper understanding of the disability community and the
               church’s obligation to reach out to those who are most vulnerable
             </li>
             <li>
-              address the issue of suffering as a Christian and implications for
+              Address the issue of suffering as a Christian and implications for
               how the Christian community views the issues of disability
             </li>
             <li>
-              learn to embrace the student’s own brokenness in a whole new way
+              Learn to embrace the student’s own brokenness in a whole new way
             </li>
           </ul>
         </div>
